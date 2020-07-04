af5c5a109939ef06b7782a6035e04c5a38d31cf6
potapovDim <potapovd3012@gmail.com>
2020-07-04 16:21:09 +0300

    add specs

--- __specs__/index.spec.ts
+++ __specs__/index.spec.ts
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {generator} from '../lib';
+import {generator, configRules} from '../lib';
 
 describe('initial', function() {
   it('defaults', function() {
@@ -17,16 +17,48 @@ describe('initial', function() {
     expect(dataGenerator.toMobile()).to.eql(dataGenerator.toWeb())
   })
 
-  it('valid', function() {
-    const generateDefaults = () => {
+  it('valids', function() {
+    const generateValids = () => {
       const generateRules = {
         field1: {valid: () => 'field1 default'},
         field2: {valid: () => 'field2 default'}
       }
       return generator({generateRules})
     }
-    const dataGenerator = generateDefaults().generate()
+    const dataGenerator = generateValids().generate()
     expect(dataGenerator.toWeb()).to.eql({field1: 'field1 default', field2: 'field2 default'})
+    expect(dataGenerator.toWeb()).to.eql(dataGenerator.toApi())
+    expect(dataGenerator.toApi()).to.eql(dataGenerator.toMobile())
+    expect(dataGenerator.toMobile()).to.eql(dataGenerator.toWeb())
+  })
+
+  it(`${configRules.empty}`, function() {
+    const generateValidsForEmpty = (config) => {
+      const generateRules = {
+        field1: {valid: () => 'field1 default'},
+        field2: {valid: () => 'field2 default'}
+      }
+      return generator({generateRules, config})
+    }
+    const config = {field1: configRules.empty};
+    const dataGenerator = generateValidsForEmpty(config).generate()
+    expect(dataGenerator.toWeb()).to.eql({field1: '', field2: 'field2 default'})
+    expect(dataGenerator.toWeb()).to.eql(dataGenerator.toApi())
+    expect(dataGenerator.toApi()).to.eql(dataGenerator.toMobile())
+    expect(dataGenerator.toMobile()).to.eql(dataGenerator.toWeb())
+  })
+
+  it(`${configRules.remove}`, function() {
+    const generateValidsForEmpty = (config) => {
+      const generateRules = {
+        field1: {valid: () => 'field1 default'},
+        field2: {valid: () => 'field2 default'}
+      }
+      return generator({generateRules, config})
+    }
+    const config = {field1: configRules.remove};
+    const dataGenerator = generateValidsForEmpty(config).generate()
+    expect(dataGenerator.toWeb()).to.eql({field2: 'field2 default'})
     expect(dataGenerator.toWeb()).to.eql(dataGenerator.toApi())
     expect(dataGenerator.toApi()).to.eql(dataGenerator.toMobile())
     expect(dataGenerator.toMobile()).to.eql(dataGenerator.toWeb())
